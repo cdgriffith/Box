466c19a6e56e1ae8c483d562044f16453c3e4540
Chris Griffith <chris@cdgriffith.com>
2019-07-05 09:47:37 -0500

    Adding license to source distribution

--- box.py
+++ box.py
@@ -45,7 +45,7 @@ else:
 __all__ = ['Box', 'ConfigBox', 'BoxList', 'SBox',
            'BoxError', 'BoxKeyError']
 __author__ = 'Chris Griffith'
-__version__ = '3.4.1'
+__version__ = '3.4.2'
 
 BOX_PARAMETERS = ('default_box', 'default_box_attr', 'conversion_box',
                   'frozen_box', 'camel_killer_box', 'box_it_up',
