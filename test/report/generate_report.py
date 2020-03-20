47108ad2543fefb367b1049b050f8ec7e1f0fad1
Chris Griffith <chris@cdgriffith.com>
2020-03-20 08:43:44 -0500

    Adding code to make pretty graphs

--- test/report/generate_report.py
+++ test/report/generate_report.py
@@ -1,3 +1,57 @@
+import sys
+from pathlib import Path
+
+from box import Box
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+
+sys.path.insert(0, Path(__file__).parent)
+
+# from run_benchmarks import main
+#
+# report = main()
+
+report = Box({'benchmarks': {'delete': 0.019694,
+                'delete_attr': 0.038236,
+                'insert': 0.587872,
+                'load': 4.4412133,
+                'lookup': 0.025484,
+                'lookup_attr': 0.068396,
+                'lookup_get': 0.039811,
+                'pop': 0.027849,
+                'set_attr': 0.415647,
+                'set_item': 0.234755,
+                'update': 0.199506,
+                'update_merge': 0.224046},
+ 'machine_specs': {'arch': ('32bit', 'WindowsPE'),
+                   'cpu_count': 16,
+                   'processor': 'Intel64 Family 6 Model 158 Stepping 12, '
+                                'GenuineIntel',
+                   'python_compiler': 'MSC v.1916 32 bit (Intel)',
+                   'python_implimentation': 'CPython',
+                   'python_version': '3.8.0'},
+ 'settings': {'box_version': '4.2.2',
+              'hearthstone_card_version': '25770',
+              'iterations': 10000,
+              'toml_version': '0.10.0',
+              'yaml_library': 'ruamel.yaml',
+              'yaml_version': '0.16.6'}})
+
+benchmarks = ('lookup', 'lookup_get', 'lookup_attr', 'pop', 'delete', 'delete_attr')
+y_pos = np.arange(len(benchmarks))
+
+plt.bar(y_pos, [report.benchmarks[x] for x in benchmarks], align='center', color=['#236267', '#236267', '#236267', '#2C8437', '#2F4172', '#2F4172'])
+plt.xticks(y_pos, benchmarks)
+plt.ylabel('Seconds')
+plt.title(f'{report.settings.iterations:,} Lookup and Delete Operations')
+plt.show()
+
+# benchmarks = ('set_item', 'set_attr', 'insert', 'update', 'update_merge')
+# y_pos = np.arange(len(benchmarks))
+#
+# plt.bar(y_pos, [report.benchmarks[x] for x in benchmarks], align='center', color=['#403075', '#403075', '#403075', '#872D62', '#872D62'])
+# plt.xticks(y_pos, benchmarks)
+# plt.ylabel('Seconds')
+# plt.title(f'{report.settings.iterations:,} Insert and Update Operations')
+# plt.show()
--- test/report/run_benchmarks.py
+++ test/report/run_benchmarks.py
@@ -90,87 +90,90 @@ def random_string():
     return key
 
 
-test = dict((random_string(), 0) for i in range(iterations))
-merge_update_dict = dict((random_string(), 0) for i in range(iterations))
-update_dict = dict((random_string(), 0) for i in range(iterations))
-set_list = [random_string() for _ in range(iterations)]
-set_attr_list = [random_string() for _ in range(iterations)]
-item_list = [random_string() for _ in range(iterations)]
+def main():
 
-card_version = '25770'
-card_cache = Path(os.path.dirname(__file__), f'hearthstone_{card_version}.json')
-report_name = f'box_{box.__version__}_report_{datetime.now().isoformat().replace(":", "-")}.json'
-if not card_cache.exists():
-    print("Downloading hearthstone card data")
-    req = requests.get(f"https://api.hearthstonejson.com/v1/{card_version}/all/cards.json")
-    print(f"Download complete, saving to cache file {card_cache}")
-    data = {'cards': req.json()}
-    card_cache.write_text(json.dumps(data))
-    print("Data saved locally for faster")
-else:
-    data = json.loads(card_cache.read_text())
-data.update(test)
+    test = dict((random_string(), 0) for i in range(iterations))
+    merge_update_dict = dict((random_string(), 0) for i in range(iterations))
+    update_dict = dict((random_string(), 0) for i in range(iterations))
+    set_list = [random_string() for _ in range(iterations)]
+    set_attr_list = [random_string() for _ in range(iterations)]
+    item_list = [random_string() for _ in range(iterations)]
 
-start_time = time.perf_counter()
-my_box = Box(data)
-load_time = time.perf_counter() - start_time
+    card_version = '25770'
+    card_cache = Path(os.path.dirname(__file__), f'hearthstone_{card_version}.json')
 
-if box.__version__.startswith('4'):
-    mu = my_box.merge_update
-else:
-    mu = my_box.update
+    if not card_cache.exists():
+        print("Downloading hearthstone card data")
+        req = requests.get(f"https://api.hearthstonejson.com/v1/{card_version}/all/cards.json")
+        print(f"Download complete, saving to cache file {card_cache}")
+        data = {'cards': req.json()}
+        card_cache.write_text(json.dumps(data))
+        print("Data saved locally for faster")
+    else:
+        data = json.loads(card_cache.read_text())
+    data.update(test)
 
-results = Box(
-    settings=Box(
-        box_version=box.__version__,
-        hearthstone_card_version=card_version,
-        yaml_library=yaml_lib,
-        yaml_version=yaml.__version__,
-        toml_version=toml.__version__,
-        iterations=iterations
-    ),
-    machine_specs=Box(
-        python_version=".".join(str(x) for x in list(sys.version_info)[:3]),
-        python_compiler=platform.python_compiler(),
-        python_implimentation=platform.python_implementation(),
-        arch=platform.architecture(),
-        processor=platform.processor(),
-        cpu_count=multiprocessing.cpu_count()
+    start_time = time.perf_counter()
+    my_box = Box(data)
+    load_time = time.perf_counter() - start_time
+
+    if box.__version__.startswith('4'):
+        mu = my_box.merge_update
+    else:
+        mu = my_box.update
+
+    results = Box(
+        settings=Box(
+            box_version=box.__version__,
+            hearthstone_card_version=card_version,
+            yaml_library=yaml_lib,
+            yaml_version=yaml.__version__,
+            toml_version=toml.__version__,
+            iterations=iterations
+        ),
+        machine_specs=Box(
+            python_version=".".join(str(x) for x in list(sys.version_info)[:3]),
+            python_compiler=platform.python_compiler(),
+            python_implimentation=platform.python_implementation(),
+            arch=platform.architecture(),
+            processor=platform.processor(),
+            cpu_count=multiprocessing.cpu_count()
+        )
     )
-)
 
-try:
-    benchmarks = Box(
-        load=load_time,
-        insert=timer(box_insert, my_box),
-        update_merge=timer(mu, merge_update_dict),
-        update=timer(my_box.update, update_dict),
-        lookup=timer(lookup, my_box),
-        lookup_get=timer(get_lookup, my_box),
-        lookup_atr=timer(attr_lookup, my_box),
-        pop=timer(pop, my_box, merge_update_dict.keys()),
-        delete=timer(delete, my_box, update_dict.keys()),
-        delete_attr=timer(delete_attr, my_box, test.keys()),
-        set_item=timer(set_item, my_box, set_list, item_list),
-        set_attr=timer(set_attr, my_box, set_attr_list, item_list),
-        # to_json=timer(my_box.to_json),
-        # to_yaml=timer(my_box.to_yaml),
-        # to_dict=timer(my_box.to_dict),
-    )
-    results.benchmarks = benchmarks
-except BoxError as err:
-    report = Path(os.path.dirname(__file__), f"failed-{report_name}.json")
-    print(f"Error while running test: {err}")
-    config = Box(test=test, item_list=item_list, update_dict=update_dict, merge_update_dict=merge_update_dict,
-                 set_list=set_list, set_attr_list=set_attr_list)
-    config.to_json(filename=Path(os.path.dirname(__file__), f"config-{report_name}.json"))
-else:
-    report = Path(os.path.dirname(__file__), report_name)
+    try:
+        benchmarks = Box(
+            load=load_time,
+            insert=timer(box_insert, my_box),
+            update_merge=timer(mu, merge_update_dict),
+            update=timer(my_box.update, update_dict),
+            lookup=timer(lookup, my_box),
+            lookup_get=timer(get_lookup, my_box),
+            lookup_attr=timer(attr_lookup, my_box),
+            pop=timer(pop, my_box, merge_update_dict.keys()),
+            delete=timer(delete, my_box, update_dict.keys()),
+            delete_attr=timer(delete_attr, my_box, test.keys()),
+            set_item=timer(set_item, my_box, set_list, item_list),
+            set_attr=timer(set_attr, my_box, set_attr_list, item_list),
+            # to_json=timer(my_box.to_json),
+            # to_yaml=timer(my_box.to_yaml),
+            # to_dict=timer(my_box.to_dict),
+        )
+        results.benchmarks = benchmarks
+    except BoxError as err:
+        print(f"Error while running test: {err}")
+        config = Box(test=test, item_list=item_list, update_dict=update_dict, merge_update_dict=merge_update_dict,
+                     set_list=set_list, set_attr_list=set_attr_list)
+        config.to_json(filename=Path(os.path.dirname(__file__), f"config-{report_name}.json"))
 
-pprint(results)
-results.to_json(filename=report, indent=2)
+    pprint(results)
+    return results
 
-# KeyError: '\'!?\x0c"='
-# '@^-+0'
-# '\x0c,`[$'
+    # KeyError: '\'!?\x0c"='
+    # '@^-+0'
+    # '\x0c,`[$'
 
+
+if __name__ == '__main__':
+    report_name = f'box_{box.__version__}_report_{datetime.now().isoformat().replace(":", "-")}.json'
+    main().to_json(filename=Path(os.path.dirname(__file__), report_name), indent=2)
